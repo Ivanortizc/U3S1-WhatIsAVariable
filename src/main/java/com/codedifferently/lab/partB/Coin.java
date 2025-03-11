4616e3b772b016bfe5b31898c3b77ab40baa3962
Ivanortizc <Ivanortizc22@gmail.com>
2025-03-11 13:15:03 -0400

    Finish

--- src/main/java/com/codedifferently/lab/partB/Coin.java
+++ src/main/java/com/codedifferently/lab/partB/Coin.java
@@ -1,4 +1,31 @@
 package com.codedifferently.lab.partB;
 
 public class Coin {
+    private String name;
+    private double value;
+
+    public Coin() {  // 
+        this.name = "Penny";
+        this.value = 0.01;
+    }
+
+    public Coin(String name, double value) {
+        this.name = name;
+        this.value = value;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+
+    public String toString() {
+        return "Coin{name='" + name + "', value =" + value +" }";
+    }
 }
+
+
